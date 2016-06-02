5ec534e9e12a15ec75ab4f0a962817c744e7307f
Derik Linch <dlinch33@gmail.com>
2016-06-02 11:22:34 -0600

    added spec for editing lists

---
+++ spec/features/todo_lists/edit_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe "Editing todo lists" do
+  let!(:todo_list) { TodoList.create(title: "Groceries", description: "Grocery List") }
+  def update_todo_list(options={})
+    options[:title] ||= "My todo list"
+    options[:description] ||= "This is my todo list."
+    todo_list = options[:todo_list]
+
+    visit "/todo_lists"
+    within "#todo_list_#{todo_list.id}" do
+      click_link "Edit"
+    end
+
+    fill_in "Title", with: options[:title]
+    fill_in "Description", with: options[:description]
+    click_button "Update Todo list"
+  end
+  it "updates a todo list successfully with the correct information" do
+    update_todo_list  todo_list: todo_list,
+                      title: "New title",
+                      description: "New description"
+
+    todo_list.reload
+
+    expect(page).to have_content("Todo list was successfully updated")
+    expect(todo_list.title).to eq("New Title")
+    expect(todo_list.description).to eq("New description")
+  end
+  
+end
