9639ab6a71f327dbe839c826be1adbd424e82583
Derik Linch <dlinch33@gmail.com>
2016-06-06 10:04:16 -0600

    added spec, route and link in view to list items

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'todo_items/index'
 
-  resources :todo_lists
+  resources :todo_lists do
+    resources :todo_items
+  end
   root 'todo_lists#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
