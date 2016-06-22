31c30701f68818897c6d012b522075784ac06363
Derik Linch <dlinch33@gmail.com>
2016-06-22 10:59:34 -0600

    added more tests

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,45 +1,65 @@
 class TodoItemsController < ApplicationController
   def index
-    @todo_list = TodoList.find(params[:todo_list_id])
+    begin
+      @todo_list = TodoList.find_by(id: params[:todo_list_id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_to new_todo_list_path
+    end
   end
 
   def new
-    @todo_list = TodoList.find(params[:todo_list_id])
-    @todo_item = @todo_list.todo_items.new
+    begin
+      @todo_list = TodoList.find_by(id: params[:todo_list_id])
+      @todo_item = @todo_list.todo_items.new
+    rescue ActiveRecord::RecordNotFound
+      redirect_to new_todo_list_path
+    end
   end
 
   def create
-    @todo_list = TodoList.find(params[:todo_list_id])
-    @todo_item = @todo_list.todo_items.new(todo_item_params)
-    if @todo_item.save
-      flash[:success] = "Added todo list item."
-      redirect_to todo_list_todo_items_path
-    else
-      flash[:error] = "There was a problem adding that todo list item."
-      render action: :new
+    begin
+      @todo_list = TodoList.find_by(id: params[:todo_list_id])
+      @todo_item = @todo_list.todo_items.new(todo_item_params)
+      if @todo_item.save
+        flash[:success] = "Added todo list item."
+        redirect_to todo_list_todo_items_path
+      else
+        flash[:error] = "There was a problem adding that todo list item."
+        render action: :new
+      end
+    rescue ActiveRecord::RecordNotFound
+      redirect_to new_todo_list_path
     end
   end
 
   def edit
-    @todo_list = TodoList.find(params[:todo_list_id])
-    @todo_item = @todo_list.todo_items.find(params[:id])
+    begin
+      @todo_list = TodoList.find_by(id: params[:todo_list_id])
+      @todo_item = @todo_list.todo_items.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_to new_todo_list_path
+    end
   end
 
   def update
-    @todo_list = TodoList.find(params[:todo_list_id])
-    @todo_item = @todo_list.todo_items.find(params[:id])
+    begin
+      @todo_list = TodoList.find_by(id: params[:todo_list_id])
+      @todo_item = @todo_list.todo_items.find(params[:id])
 
-    if @todo_item.update_attributes(todo_item_params)
-      flash[:success] = "Saved todo list item."
-      redirect_to todo_list_todo_items_path
-    else
-      flash[:error] = "That todo item could not be saved."
-      render action: :edit
+      if @todo_item.update_attributes(todo_item_params)
+        flash[:success] = "Saved todo list item."
+        redirect_to todo_list_todo_items_path
+      else
+        flash[:error] = "That todo item could not be saved."
+        render action: :edit
+      end
+    rescue ActiveRecord::RecordNotFound
+      redirect_to new_todo_list_path
     end
   end
 
   def url_options
-    { todo_list_id: params[:todo_list_id] }.merge(super)
+    {todo_list_id: params[:todo_list_id]}.merge(super)
   end
 
   private
--- spec/controllers/todo_items_controller_spec.rb
+++ spec/controllers/todo_items_controller_spec.rb
@@ -1,5 +1,68 @@
 require 'spec_helper'
 
 describe TodoItemsController do
+  let(:todo_item) {TodoItem.create(content: "Get milk and eggs")}
+  let(:todo_list) {TodoList.create(title: "My Title", description: "This is my test list")}
+  before() do
+    todo_list.todo_items << todo_item
+  end
 
+  describe "#index" do
+    it 'should set the @todo_list controller based off the todo_list_id' do
+      get :index, todo_list_id: todo_list.id
+
+      expect(assigns(:todo_list)).to eq(todo_list)
+    end
+
+    it 'should redirect to new_todo_list_path if todo_list is not found' do
+      get :index, todo_list_id: todo_list.id + 1
+      expect(response).to redirect_to(new_todo_list_path)
+    end
+
+    it 'should render the index template' do
+      get :index, todo_list_id: todo_list.id
+      expect(response).to render_template(:index)
+    end
+  end
+
+  describe "#new" do
+    it 'should render the new template' do
+      get :new, todo_list_id: todo_list.id
+      expect(response).to render_template(:new)
+    end
+
+    it 'should redirect to new_todo_list_path if todo_list is not found' do
+      get :new, todo_list_id: todo_list.id + 1
+      expect(response).to redirect_to(new_todo_list_path)
+    end
+  end
+
+  describe "#edit" do
+    it 'should render the edit template' do
+      get :edit, todo_list_id: todo_list.id, id: todo_item.id
+      expect(response).to render_template(:edit)
+    end
+
+    it 'should redirect to new_todo_list_path if todo_list is not found' do
+      get :edit, todo_list_id: todo_list.id + 1, id: todo_item.id
+      expect(response).to redirect_to(new_todo_list_path)
+    end
+  end
+
+  describe "#update" do
+    it 'should redirect to the index' do
+      put :update, todo_list_id: todo_list.id, id: todo_item.id, todo_item: {content: todo_item.content}
+      expect(response).to redirect_to(todo_list_todo_items_path)
+    end
+
+    it 'should redirect to new_todo_list_path if todo_list is not found' do
+      put :update, todo_list_id: todo_list.id + 1, id: todo_item.id
+      expect(response).to redirect_to(new_todo_list_path)
+    end
+
+    it 'should update the specified item' do
+      put :update, todo_list_id: todo_list.id, id: todo_item.id, todo_item: {content: "New content"}
+      expect(todo_item.reload.content).to eq "New content"
+    end
+  end
 end
