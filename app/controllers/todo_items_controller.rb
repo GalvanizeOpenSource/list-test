c10a1fe2a6e9df9b389c6d92c40c77df468bce29
Jeff Chang <jeffrey.chang2014@gmail.com>
2016-07-26 13:55:28 -0700

    Change call to find

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -43,7 +43,7 @@ class TodoItemsController < ApplicationController
 
   def update
     begin
-      @todo_list = TodoList.find_by(id: params[:todo_list_id])
+      @todo_list = TodoList.find(params[:todo_list_id])
       @todo_item = @todo_list.todo_items.find(params[:id])
 
       if @todo_item.update_attributes(todo_item_params)
