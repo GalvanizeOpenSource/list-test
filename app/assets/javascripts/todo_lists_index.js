f7b445656932dcc92e65de4298940886d20113f2
Jeff Chang <jeffrey.chang2014@gmail.com>
2016-07-26 19:27:35 -0700

    Add ajax call to update item with done

--- app/assets/javascripts/todo_lists_index.js
+++ app/assets/javascripts/todo_lists_index.js
@@ -9,7 +9,29 @@ $(document).ready(function(){
   });
 
   $('button').click(function(){
-    $(this).closest('li').css('background-color', '#E0EEE0');
-    $(this).replaceWith('Done!');
+    var button = $(this);
+    var list_id = $('ul').attr('id').replace('todo_list_','');
+    var item = button.closest('li');
+    var item_id = item.attr('id').replace('todo_item_','');
+    var url = '/todo_lists/'+list_id+'/todo_items/'+item_id;
+
+    $.ajax({
+      url: url,
+      type: "PATCH",
+      data: {
+        "todo_item": {done: true}
+      },
+      dataType: "json"
+    }).done(function(data) {
+      if (data['success']) {
+        item.addClass('done');
+        button.replaceWith('Done!');
+      } else {
+        $('.flash').text(data['error']);
+      }
+    }).fail(function(request, status, error) {
+      $('.flash').text('Sorry, we could not mark your todo item as done.')
+      console.log(error);
+    });
   });
 });
