6cf01412ef9b6adfaa3b5282fc4d8ce1b395386e
Jared Koumentis <jared@koumentis.com>
2016-08-19 19:25:22 -0600

    [FEAT] Adds ability to add due date and time to todo item

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -65,7 +65,7 @@ class TodoItemsController < ApplicationController
   private
 
   def todo_item_params
-    params[:todo_item].permit(:content)
+    params[:todo_item].permit(:content, :due_datetime)
   end
 
 end
