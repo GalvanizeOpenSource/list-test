5666d6a6d8a91c568ec55e3424faf8f2d5b4d24d
James Dullaghan <james@fldlvl.com>
2016-09-12 18:26:02 -0600

    Refactor some of the controller
    move some todo item form logic into helper
    
    Refactor todo_item into collection partial

--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -4,5 +4,10 @@ class TodoItem < ActiveRecord::Base
   validates :content, presence: true,
                       length: { minimum: 2 }
 
-  validates_datetime :due_by, on_or_after: -> { DateTime.current }
+  validates_datetime :due_by, on_or_after: -> { DateTime.current },
+                              allow_nil: true,
+                              allow_blank: true
+
+  scope :order_alphabetically, -> { where("due_by IS NULL OR due_by IS ''").order(content: :desc) }
+  scope :order_due_by, ->         { where("due_by IS NOT NULL OR due_by <> ''").order(due_by: :asc) }
 end
