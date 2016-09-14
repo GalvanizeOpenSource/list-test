57a07b107aea1ab00e8d987ede50fc412e58adc0
James Dullaghan <james@fldlvl.com>
2016-09-13 18:02:47 -0600

    Add todo items request spec

--- app/controllers/api/todo_items_controller.rb
+++ app/controllers/api/todo_items_controller.rb
@@ -1,26 +1,30 @@
 module Api
   class TodoItemsController < ::ApplicationController
     def create
+      return render_error if todo_list.nil?
+
       todo_item = todo_items.new(permitted_params)
 
       if todo_item.save
         default_message = "Saved todo list item."
         render json: { todo_item: todo_item, message: default_message }, status: :ok
       else
-        render json: { errors: todo_item.errors.full_messages }, status: :unprocessible_entity
+        render json: { errors: todo_item.errors.full_messages }, status: :unprocessable_entity
       end
-    rescue NoMethodError, ActiveRecord::RecordNotFound
-      render json: { errors: "Unable to find todo list to add item to." }, status: :unprocessible_entity
     end
 
     private
+
+    def render_error
+      render json: { errors: "Unable to find todo list to add item to." }, status: :unprocessable_entity
+    end
 
     def permitted_params
       params.permit(:content)
     end
 
     def todo_items
-      @todo_items ||= todo_list.todo_items
+      @todo_items ||= todo_list.todo_items if todo_list
     end
 
     def todo_list
