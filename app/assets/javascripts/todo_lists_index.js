839e82e290c9f3437e7c9bb0c2896cdbd2624aca
James Dullaghan <james@fldlvl.com>
2016-09-14 22:40:53 -0600

    Refactor todo item submission

--- app/assets/javascripts/todo_lists_index.js
+++ app/assets/javascripts/todo_lists_index.js
@@ -1,3 +1,39 @@
+var TodoItem = function(selector) {
+  this.selector = selector || '.submit-button';
+  this.content = $('#content');
+
+  this.submit = function() {
+    var that = this;
+
+    $(this.selector).click(function(e) {
+
+      var url = that.content.data('url');
+      var data = { todo_list_id: that.content.data('parentId'), content: that.content.val() };
+
+      if (that.content.length > 0) {
+        $.ajax({
+          url: url,
+          dataType: 'json',
+          method: "POST",
+          data: data
+        }).done(function(res) {
+          // Reset form field
+          that.content.val('');
+          that.content.next().replaceWith("<ul>" + res.message + "</ul>");
+
+        }).fail(function(jqXHR) {
+          var errors = jqXHR.responseJSON.errors;
+          var errorHtml = errors.map(function(message, index) {
+            return "<li>" + message + "</li>";
+          });
+
+          that.content.after("<ul>" + errorHtml.join('') + "</ul>");
+        });
+      }
+    });
+  }
+}
+
 $(document).ready(function(){
   var modal = new Modal('#openModal');
 
@@ -16,31 +52,6 @@ $(document).ready(function(){
     modal.close();
   });
 
-  $('.submit-button').click(function(e) {
-    var $content = $('#content');
-
-    var url = $content.data('url');
-    var data = { todo_list_id: $content.data('parentId'), content: $content.val() };
-
-    if ($content.length > 0) {
-      $.ajax({
-        url: url,
-        dataType: 'json',
-        method: "POST",
-        data: data
-      }).done(function(res) {
-        // Reset form field
-        $content.val('');
-        $content.next().replaceWith("<ul>" + res.message + "</ul>");
-
-      }).fail(function(jqXHR) {
-        var errors = jqXHR.responseJSON.errors;
-        var content = errors.map(function(message, index) {
-          return "<li>" + message + "</li>";
-        });
-
-        $content.after("<ul>" + content.join('') + "</ul>");
-      });
-    }
-  });
+  var todoItem = new TodoItem;
+  todoItem.submit();
 });
