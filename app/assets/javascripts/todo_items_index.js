6c0207c1cbf597264913ddf47db7d8d635c26e4c
Daniel McGraw <dan.j.mcgraw@gmail.com>
2016-10-05 19:02:58 -0600

    Add ajax call to destroy for the given item.

--- app/assets/javascripts/todo_items_index.js
+++ app/assets/javascripts/todo_items_index.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
 
-  $(document).on('click', '.delete-item', function(){
+  $(document).on('click', '.delete-item', function(e){
+    e.preventDefault();
+    $('.confirm-delete').data('itemId', $(e.target).data('itemId'));
     $('#openModal').addClass('show');
   });
 
   $(document).on('click', '.confirm-delete', function() {
+    $.ajax({
+      url: '/todo_lists/' + $('.confirm-delete').data('listId') + '/todo_items/' + $('.confirm-delete').data('itemId'),
+      type: 'DELETE'
+    });
     $('#openModal').removeClass('show');
   });
 
